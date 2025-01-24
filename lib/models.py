13e535ad0d22e6cffc24cd749b86e5cade61a399
enock-jufri <enockjufri@gmail.com>
2025-01-24 13:50:03 +0300

    sql alchemy

--- lib/models.py
+++ lib/models.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import (Column, String, Integer)
+from sqlalchemy import (Column, String, Integer,create_engine)
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
@@ -11,3 +12,5 @@ class Dog(Base):
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     breed = Column(String())
+    
+   
